9a30463cac990e912d0feb31250c3d80e4a331a2
Gabriel Ribeiro Fernandes dos Santos <12117236@aluno.newtonpaiva.br>
2022-10-13 18:57:23 -0300

    Segundo commit

--- Ex1.java
+++ Ex1.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
 public class Ex1 {
+    public static void main(String[] args) {
+        float nota;
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite uma nota: ");
+        nota = ler.nextFloat();
+
+        if (nota < 40)
+            System.out.println("O aluno esta reprovado.");
+        else if (nota >= 40 && nota < 60)
+            System.out.println("O aluno esta de recuperação.");
+        else
+            System.out.println("O aluno esta aprovado!");
+    }
+
 }
---
+++ Ex2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        int dia;
+        System.out.printf("Digite um dia da semana ");
+        Scanner ler = new Scanner(System.in);
+        dia = ler.nextInt();
+
+        switch(dia){
+            
+
+
+        }
+    }
+
+
+
+
+
+
+}
